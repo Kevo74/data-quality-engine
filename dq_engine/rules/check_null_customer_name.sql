12e14788223b4c88128ead8b3c0c2215423733d3
Kevo74 <74kevo@gmail.com>
2026-07-03 12:38:31 -0500

    Initializing Dq Repo

---
+++ dq_engine/rules/check_null_customer_name.sql
@@ -0,0 +1,9 @@
+-- Rule: Check for NULL cusstomer_name values
+-- Purpose: Ensure customer_name is always populated fo analytics
+
+SELECT
+    COUNT(*) AS null_customer_name_count
+FROM 
+    customers
+WHERE 
+    customer_name IS NULL;
---
+++ dq_engine/rules/check_null_order_date.sql
@@ -0,0 +1,9 @@
+-- Rule: Check for Null order_date values
+-- Purpose: Ensure all orders have a valid order_date
+
+SELECT
+    COUNT(*) AS null_order_date_count  
+FROM 
+    orders
+WHERE 
+    order_date IS NULL;
